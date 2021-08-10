ddb9a30adea5e68133bb3e4115b4ed6a6cf58e62
Kieran BW <41634689+FredHappyface@users.noreply.github.com>
2021-08-10 23:20:49 +0100

    cleanup

--- app/src/main/java/com/fredhappyface/ewesticker/ImageKeyboard.kt
+++ app/src/main/java/com/fredhappyface/ewesticker/ImageKeyboard.kt
@@ -7,7 +7,6 @@ import android.graphics.ImageDecoder
 import android.graphics.drawable.AnimatedImageDrawable
 import android.graphics.drawable.Drawable
 import android.inputmethodservice.InputMethodService
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
