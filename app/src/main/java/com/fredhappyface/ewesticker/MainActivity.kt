0850d3d2ddfb88c65ab8f7b4c13723948e9a5864
Kieran BW <41634689+FredHappyface@users.noreply.github.com>
2021-10-26 21:52:03 +0100

    null safety improvements

--- app/src/main/java/com/fredhappyface/ewesticker/MainActivity.kt
+++ app/src/main/java/com/fredhappyface/ewesticker/MainActivity.kt
@@ -23,6 +23,9 @@ import java.nio.file.Files
 import java.util.*
 import java.util.concurrent.Executors
 
+/**
+ * MainActivity class inherits from the AppCompatActivity class - provides the settings view
+ */
 class MainActivity : AppCompatActivity() {
 	// init
 	private val mSupportedMimes = Utils.getSupportedMimes()
