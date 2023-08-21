525679fe34c4b1bd5647278f9f8cba3a3d5e5868
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2023-08-21 19:58:05 +0100

    code enhancements and refactor

--- app/src/main/java/com/fredhappyface/ewesticker/utilities/SharedPrefHelper.kt
+++ app/src/main/java/com/fredhappyface/ewesticker/utilities/SharedPrefHelper.kt
@@ -1,38 +1,25 @@
 package com.fredhappyface.ewesticker.utilities
 
 import android.content.Context
-import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
 import com.fredhappyface.ewesticker.model.StickerPack
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
 
-private const val SHARED_PREF = "com.fredhappyface.ewesticker.pref"
-private const val KEY_STICKERS = "com.fredhappyface.ewesticker.pref.stickers"
+object SharedPrefHelper {
+	private const val KEY_STICKERS = "com.fredhappyface.ewesticker.pref.stickers"
 
-class SharedPrefHelper {
-	fun getSharedPreferences(context: Context): SharedPreferences {
-		return context.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
-	}
-
-
-	fun getStickerPacksFromPref(context: Context): ArrayList<StickerPack?> {
-		val sharedPreferences =
-			context.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
+	fun getStickerPacksFromPref(context: Context): List<StickerPack> {
+		val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 		val gson = Gson()
-		val json = sharedPreferences.getString(KEY_STICKERS, "")
-		val stickerPacks: ArrayList<StickerPack?> = if (json == "") {
-			ArrayList()
-		} else {
-			try {
-				gson.fromJson(
-					sharedPreferences.getString(KEY_STICKERS, null),
-					object : TypeToken<ArrayList<StickerPack>>() {}.type
-				)
-			} catch (e: JsonSyntaxException) {
-				ArrayList()
-			}
+		val json = sharedPreferences.getString(KEY_STICKERS, null) ?: return emptyList()
+
+		return try {
+			gson.fromJson(json, object : TypeToken<ArrayList<StickerPack>>() {}.type)
+		} catch (e: JsonSyntaxException) {
+			emptyList()
 		}
-		return stickerPacks
 	}
+
 }
--- app/src/main/java/com/fredhappyface/ewesticker/view/StickerPackViewHolder.kt
+++ app/src/main/java/com/fredhappyface/ewesticker/view/StickerPackViewHolder.kt
@@ -1,8 +1,9 @@
-package com.fredhappyface.ewesticker
+package com.fredhappyface.ewesticker.view
 
 import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.fredhappyface.ewesticker.R
 
 class StickerPackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 	val stickerThumbnail: ImageView = itemView.findViewById(R.id.stickerButton)
