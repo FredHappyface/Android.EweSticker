43cda5728e4e31e177ab5df642614ff7233cdab2
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2025-02-17 18:31:52 +0000

    fmt

--- app/src/main/java/com/fredhappyface/ewesticker/utilities/StartLogger.kt
+++ app/src/main/java/com/fredhappyface/ewesticker/utilities/StartLogger.kt
@@ -2,17 +2,17 @@ package com.fredhappyface.ewesticker.utilities
 
 import com.elvishew.xlog.LogConfiguration
 import com.elvishew.xlog.LogLevel
+import com.elvishew.xlog.XLog
 import com.elvishew.xlog.printer.AndroidPrinter
 import com.elvishew.xlog.printer.file.FilePrinter
 import com.elvishew.xlog.printer.file.naming.DateFileNameGenerator
 import java.io.File
-import com.elvishew.xlog.XLog
 
 
 fun startLogger(filesDir: File) {
 
 	try {
-	    XLog.i("startLogger if not already started")
+		XLog.i("startLogger if not already started")
 	} catch (e: IllegalStateException) {
 
 		val logConfig = LogConfiguration.Builder().logLevel(LogLevel.ALL).tag("EweSticker").build()
